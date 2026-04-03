0b7a06835863c1ca398ff46d4c3697321e5095c4
shayaf84 <shayafmd@gmail.com>
2026-04-03 11:59:15 -0400

    Add nodes and factors to graph

---
+++ src/PoseGraph.cpp
@@ -0,0 +1,27 @@
+#include "PoseGraph.hpp"
+
+PoseGraph::PoseGraph() {
+
+}
+
+int PoseGraph::addNode(const Pose2 &pose_estimate) {
+    int id = nodes_.size();
+    PoseGraphNode toAdd = PoseGraphNode{id, pose_estimate};
+    nodes_.push_back(toAdd);
+    return id;
+}
+
+void PoseGraph::addPriorFactor(int node_id, const Pose2 &measurement, const Eigen::Matrix3d &information) {
+    PriorFactor toAdd = PriorFactor{node_id, measurement, information};
+    priors_.push_back(toAdd);
+}
+
+void PoseGraph::addOdometryFactor(int from_id, int to_id, const Pose2 &measurement, const Eigen::Matrix3d &information) {
+    PoseGraphEdge toAdd = PoseGraphEdge{from_id, to_id, measurement, information, FactorType::Odometry};
+    edges_.push_back(toAdd);
+}
+
+void PoseGraph::addLoopClosureFactor(int from_id, int to_id, const Pose2 &measurement, const Eigen::Matrix3d &information) {
+    PoseGraphEdge toAdd = PoseGraphEdge{from_id, to_id, measurement, information, FactorType::LoopClosure};
+    edges_.push_back(toAdd);
+}
